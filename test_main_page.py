b63f1d28d19f1880df17252306e3fa3292764143
Sergey Smirnov <pariss8@mail.ru>
2020-03-05 15:21:47 +0300

    разбиваем функцию на две

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,14 @@
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login_page(browser)
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
 
