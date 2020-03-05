07e7efd81d94e6b37a74b616967292f8ed2a6e1d
Sergey Smirnov <pariss8@mail.ru>
2020-03-05 16:54:19 +0300

    Added is_element_present to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
     browser: webdriver.Chrome
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
+        self.browser.implicitly_wait(timeout)
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
 
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -9,3 +9,6 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+
