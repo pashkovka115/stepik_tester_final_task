1938a7a905d9d5096b4d8c948f333f1510863cba
Sergey Smirnov <pariss8@mail.ru>
2020-03-05 15:38:38 +0300

    выделен базовый класс

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+class BasePage:
+    browser: webdriver.Chrome
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
