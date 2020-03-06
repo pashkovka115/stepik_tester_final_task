c5931679cb68f2a40b3cd2fdaf9f2f3e7f1334cd
Sergey Smirnov <pariss8@mail.ru>
2020-03-06 13:55:54 +0300

    Add waits

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,6 @@
 import math
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -22,10 +22,32 @@ class BasePage:
 
 
     def is_element_present(self, how, what):
+        """ Элемент появится в течении времени не явного ожидания """
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
+        return True
+
+
+    def is_not_element_present(self, how, what, timeout=4):
+        """ Элемент не появляется в течении timeout секунд """
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        """ Элемент исчезает в течении timeout секунд """
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
 
 
