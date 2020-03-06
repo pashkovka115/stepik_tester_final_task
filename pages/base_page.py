9acc5d5b65f7dd939892a658a9c9556ccc5e9abc
Sergey Smirnov <pariss8@mail.ru>
2020-03-06 08:14:24 +0300

    modifay BasePage for dz

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,20 @@
+import math
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 class BasePage:
     browser: webdriver.Chrome
+
 
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.browser.implicitly_wait(timeout)
         self.url = url
+
 
     def open(self):
         self.browser.get(self.url)
@@ -19,3 +26,18 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
